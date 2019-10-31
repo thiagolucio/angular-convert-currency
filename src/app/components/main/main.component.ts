ad2b9c78196272ad1c0466f7a71f58593e52fb10
thiagolucio <thiagolucio@gmail.com>
2019-10-31 19:51:52 -0300

    adicionando as funcoes dos components

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MainService } from './main.service';
 import { Moedas } from 'src/app/interfaces/moedas';
-import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators';
-
 
 @Component({
   selector: 'app-main',
@@ -18,28 +15,30 @@ export class MainComponent implements OnInit {
 
   ngOnInit() {
     this.getMoedas();
+    this.getMoedasUm();
     console.log('LISTAGEM DE MOEDAS', this.moedas);
   }
+    // Quando eu habilito este aqui ele informa (no html) que USD, EUR e BTC são undefined ou não existe no Moedas[]
+    getMoedas() {
+      this.mainService.getListaMoedas()
+        .subscribe(
+          (moedas: Moedas[]) => {
+          this.moedas = moedas;
+          console.log('Os dados foram capturados com sucesso', this.moedas);
+        }, () => {console.log('Erro ao carregar dados da API Moedas');
+      });
+    }
 
-  getMoedas() {
+  // Quando eu habilito este abaixo a propriedade name, high, etc. Ficam acusando serem undefined
+  getMoedasUm(): void {
     this.mainService.getListaMoedas()
       .subscribe(
-        (moedas: Moedas[]) => {
-        this.moedas = moedas;
-        console.log('Os dados foram capturados com sucesso', this.moedas);
+        (moedas: any) => {
+        this.moedas = moedas.USD;
+        console.log('Os dados foram capturados com sucesso', this.moedas.USD);
       }, () => {console.log('Erro ao carregar dados da API Moedas');
     });
   }
-
-  // getMoedas(): void {
-  //   this.mainService.getListaMoedas()
-  //     .subscribe(
-  //       (moedas: any) => {
-  //       this.moedas = moedas.USD;
-  //       console.log('Os dados foram capturados com sucesso', this.moedas.USD);
-  //     }, () => {console.log('Erro ao carregar dados da API Moedas');
-  //   });
-  // }
 
   // calculoUsd() {
   //   this.resultDollar = this.valorDigitado * this.moedas.USD.high;
